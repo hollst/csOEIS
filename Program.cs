cf3db96e87b0eb14be3909bcf7446c30cbb89566
Frank Hollstein <frankhollstein@web.de>
2019-04-14 10:29:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ namespace csOEIS
         {
             A000045 a000045 = new A000045();
             a000045.Sequence.SequenceToString().EndMessageLine();
-
+            
+            A022441 a022441 = new A022441();
+            for (var i = 0; i < a022441.Sequence.Length; i++)
+                WriteLine($"i: {i,4} {a022441.Sequence[i].ToString("n0"),8}");
+            "a022441 ready".EndMessageLine();
+            
             decimal.MaxValue.ToString("n0").EndMessageLine();
         }
     }
